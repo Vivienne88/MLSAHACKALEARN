466e57c700c47f87f30303c46ef105a053141df0
vivienne88 <babobigi@naver.com>
2021-08-05 21:02:03 +0900

    Add api

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
+export default class App extends React.Component {
+  async getData() {
+    const res = await axios.get('http://localhost:7071/api/datetimes');
+    return res.data.message; 
+}
+
+constructor(...args) {
+  super(...args);
+  this.state = {data: null};
+}
+
+componentDidMount() {
+  if (!this.state.data) {
+      (async () => {
+          try {
+             this.setState({data: await this.getData()});
+          } catch (e) {
+          }
+      })();
+  }
+
+}
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HackaLearn Korea_member1
+          MLSA_HackaLearn
         </p>
         <a
           className="App-link"
@@ -15,11 +40,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Time : {this.state.data}
         </a>
       </header>
     </div>
   );
-}
 
-export default App;
+  }
+
+  }
